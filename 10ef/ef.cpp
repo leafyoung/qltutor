a995ad73121d2afb1cb88fbfa723f5c8808fc4dd
Yang Ye <yy@runchee.com>
2014-10-24 10:35:20 +0800

    added 10 efficient frontier

--- 10ef/ef.cpp
+++ 10ef/ef.cpp
@@ -26,18 +26,26 @@
 //        z is vector leading to result x
 //        x = {x1...xn}, Sum(x) = 1, xi = zi/sum(Zi)
 
-using namespace QuantLib;
-
 namespace {
   using namespace QuantLib;
-  double calculatePortfolioReturn(double proportionA, double expectedReturnA, double expectedReturnB) {
+
+  double calculatePortfolioReturn
+    ( double proportionA
+    , double expectedReturnA
+    , double expectedReturnB )
+  {
     return proportionA * expectedReturnA + (1-proportionA) * expectedReturnB;
   }
 
-  Volatility calculatePortfolioRisk(double proportionA, Volatility volatilityA, Volatility volatilityB, double covarianceAB) {
-    return std::sqrt( std::pow(proportionA, 2) * std::pow(volatilityA,2)
+  Volatility calculatePortfolioRisk
+    ( double     proportionA
+    , Volatility volatilityA
+    , Volatility volatilityB
+    , double     covarianceAB )
+  {
+    return std::sqrt( std::pow(proportionA,     2) * std::pow(volatilityA,2)
                     + std::pow(1 - proportionA, 2) * std::pow(volatilityB,2)
-                    + 2 * proportionA * (1-proportionA) * covarianceAB );
+                    + 2 * proportionA * (1 - proportionA) * covarianceAB );
   }
 
   BOOST_AUTO_TEST_CASE(testEfficientFrontier) {
@@ -86,13 +94,14 @@ namespace {
       portfolioReturnVectorMinusC[i][0] = portfolioReturnVector[i][0] - c;
     }
 
-    std::cout << boost::format("Portfolio return vector minus constantrate (c = %f)") % c << std::endl;
+    std::cout << boost::format("Portfolio return vector minus constraints (c = %f)") % c << std::endl;
     std::cout << portfolioReturnVectorMinusC << std::endl;
 
     //inverse of covariance matrix
     const Matrix inverseOfCovarMatrix = inverse(covarianceMatrix);
 
-    //z vector
+    //z vectors
+    //Az: just with return vector
     const Matrix& portfolioAz = inverseOfCovarMatrix * portfolioReturnVector;
     std::cout << "Portfolio Az vector" << std::endl;
     std::cout << portfolioAz << std::endl;
@@ -101,6 +110,7 @@ namespace {
       sumOfPortfolioAz += n;
     });
 
+    //Bz: just with return vector minus C
     const Matrix& portfolioBz = inverseOfCovarMatrix * portfolioReturnVectorMinusC;
     std::cout << "Portfolio Bz vector" << std::endl;
     std::cout << portfolioBz << std::endl;
@@ -110,18 +120,14 @@ namespace {
     });
 
     //portfolio weights
-    Matrix weightsPortfolioA(4,1);
+    Matrix weightsPortfolioA(4,1), weightsPortfolioB(4,1);
     for (int i=0; i<4; ++i) {
       weightsPortfolioA[i][0] = portfolioAz[i][0]/sumOfPortfolioAz;
+      weightsPortfolioB[i][0] = portfolioBz[i][0]/sumOfPortfolioBz;
     }
 
     std::cout << "Portfolio A weights" << std::endl;
     std::cout << weightsPortfolioA << std::endl;
-
-    Matrix weightsPortfolioB(4,1);
-    for (int i=0; i<4; ++i) {
-      weightsPortfolioB[i][0] = portfolioBz[i][0]/sumOfPortfolioBz;
-    }
 
     std::cout << "Portfolio B weights" << std::endl;
     std::cout << weightsPortfolioB << std::endl;
@@ -147,14 +153,15 @@ namespace {
 
     //covariance and correlation of returns
     const Matrix& covarianceABMatrix = transpose(weightsPortfolioA) * covarianceMatrix * weightsPortfolioB;
-    double covarianceAB = covarianceABMatrix[0][0];
-    double correlationAB = covarianceAB/(stdDeviationPortfolioA * stdDeviationPortfolioB);
+    double covarianceAB  = covarianceABMatrix[0][0];
+    double correlationAB = covarianceAB / stdDeviationPortfolioA / stdDeviationPortfolioB;
     std::cout << boost::format("Covariance of portfolio A and B: %f") % covarianceAB << std::endl;
     std::cout << boost::format("Correlation of portfolio A and B: %f") % correlationAB << std::endl;
 
     //generate envelope set of portfolios
     double startingProportion = -.40;
     double increment = .10;
+    // Proportion => (Volatility, return)
     std::map<double, std::pair<Volatility, double> > mapOfProportionToRiskAndReturn;
     std::map<Volatility, double> mapOfVolatilityToReturn;
     for (int i = 0; i < 21; ++i) {
@@ -176,17 +183,20 @@ namespace {
     envelopeSetFile.close();
 
     //find minimum risk portfolios on efficient frontier
+    //minimum volatility/risk
     std::pair<Volatility, double> minimumVariancePortfolioRiskAndReturn = *mapOfVolatilityToReturn.begin();
     Volatility minimumRisk = minimumVariancePortfolioRiskAndReturn.first;
     double maximumReturn = minimumVariancePortfolioRiskAndReturn.second;
     std::cout << boost::format("Maximum portfolio return for risk of %f is %f") % minimumRisk % maximumReturn << std::endl;
 
     //generate efficient frontier
+    //stop at minimum risk
     std::map<Volatility, double> efficientFrontier;
     for ( std::map<double, std::pair<Volatility, double> >::const_iterator i = mapOfProportionToRiskAndReturn.begin()
         ; i != mapOfProportionToRiskAndReturn.end()
         ; ++i ) {
       efficientFrontier[i->second.first] = i->second.second;
+      std::cout << boost::format("%f,%f,%f,%f") % i->first % i->second.first % i->second.second % minimumRisk << std::endl;
       if (i->second.first == minimumRisk) break;
     }
 
